24056fe29c847f949e313ae15fbfb626941bf217
Le Nguyen Phuong Thai <thailnp1303@gmail.com>
2022-10-25 23:23:54 +0000

    Giao dien UI JavaScript

---
+++ app.js
@@ -0,0 +1,17 @@
+// bài 1
+let inputSalary = parseFloat(document.querySelector('#inputSalary').value);
+let inputWorkingDay = parseFloat(document.querySelector('#inputWorkingDay').value);
+
+function sumSalary(number1,number2){
+  return number1*number2;
+}
+
+// func gọi nút tính lương
+function resultSalary(){
+  
+  // let result = sumSalary(inputSalary,inputWorkingDay);
+  // console.log(result);
+  // document.querySelector('#resultSalary').value = result;
+
+}
+resultSalary();
